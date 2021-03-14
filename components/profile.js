a41ef56228a01a9cb6ed94402bcff9ba64a56a50
Jennifer Bland <jennifer.bland@voxmedia.com>
2021-03-13 22:18:05 -0500

    first commit

---
+++ components/profile.js
@@ -0,0 +1,7 @@
+import Image from 'next/image'
+
+export default function Profile() {
+    return (
+        <Image src="/images/profile.jpg" height={400} width={400} alt="Jennifer Bland" />
+    )
+}
